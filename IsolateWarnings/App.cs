4dfd68b45401b41b25b76829b5f53438e24be1b4
Russ Green <1886088+russgreen@users.noreply.github.com>
2022-03-07 08:34:38 +0000

    First commit to Github

---
+++ IsolateWarnings/App.cs
@@ -0,0 +1,100 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace IsolateWarnings
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    class App : IExternalApplication
+    {
+        public static UIControlledApplication cachedUiCtrApp;
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            cachedUiCtrApp = application;
+            var ribbonPanel = CreateRibbonPanel();
+
+            return Result.Succeeded;
+        }
+
+        private RibbonPanel CreateRibbonPanel()
+        {
+            RibbonPanel panel;
+
+            //Check if "Archisoft Tools" already exists and use if its there
+            try
+            {
+                panel = cachedUiCtrApp.CreateRibbonPanel("Archisoft Tools", Guid.NewGuid().ToString());
+                panel.Name = "ARBG_IsolateWarnings_ExtApp";
+                panel.Title = "Isolate Warnings";
+            }
+            catch
+            {
+                var archisoftPanel = false;
+                var pluginPath = @"C:\ProgramData\Autodesk\ApplicationPlugins";
+                if (System.IO.Directory.Exists(pluginPath) == true)
+                {
+                    foreach (var folder in System.IO.Directory.GetDirectories(pluginPath))
+                    {
+                        if (folder.ToLower().Contains("archisoft") == true & folder.ToLower().Contains("archisoft isolate warnings") == false)
+                        {
+                            archisoftPanel = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (archisoftPanel == true)
+                {
+                    cachedUiCtrApp.CreateRibbonTab("Archisoft Tools");
+                    panel = cachedUiCtrApp.CreateRibbonPanel("Archisoft Tools", Guid.NewGuid().ToString());
+                    panel.Name = "ARBG_IsolateWarnings_ExtApp";
+                    panel.Title = "Isolate Warnings";
+                }
+                else
+                {
+                    panel = cachedUiCtrApp.CreateRibbonPanel("Isolate Warnings");
+                }
+            }
+
+            PushButtonData pbData = new PushButtonData("Isolate Warnings", "Isolate Warnings", Assembly.GetExecutingAssembly().Location, "IsolateWarnings.cmdWarnings");
+            PushButton pb = (PushButton)panel.AddItem(pbData);
+            pb.ToolTip = "Isolate elements with warnings in Revit";
+            pb.LargeImage = PngImageSource("IsolateWarnings.Images.Warnings32.png");
+
+
+            ContextualHelp contextHelp = new ContextualHelp(ContextualHelpType.Url, @"https://github.com/russgreen/IsolateWarnings/wiki");
+            pb.SetContextualHelp(contextHelp);
+
+            return panel;
+        }
+
+        private System.Windows.Media.ImageSource PngImageSource(string embeddedPath)
+        {
+            var stream = GetType().Assembly.GetManifestResourceStream(embeddedPath);
+            System.Windows.Media.ImageSource imageSource;
+            try
+            {
+                imageSource = BitmapFrame.Create(stream);
+            }
+            catch
+            {
+                imageSource = null;
+            }
+
+            return imageSource;
+        }
+    }
+}
