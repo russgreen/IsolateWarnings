c40bab638d250f11c48785277009080378d817a6
Russ Green <1886088+russgreen@users.noreply.github.com>
2024-03-31 11:10:27 +0100

    `Revamp build process and update project configurations`
    Significant changes have been made to the build process and project configurations. The `Isolate Warnings.aip` file has been removed, indicating a change in the software's installation process. The `.gitignore` file and directory structure have been updated. A new project "_build" has been added to the solution, and solution configurations have been updated. New build scripts for different platforms and a JSON schema file for build configuration have been added. The ".editorconfig" file has been updated with new C# code style rules. New build tasks have been added and the main build file has been updated to include these tasks. New dependencies and targets have been added in `Build.Installer.cs` and `Build.Publish.cs`. Helper methods for the installer building and signing process have been defined. A new class `Configuration` has been added to represent build configurations. New `Directory.Build.props` and `Directory.Build.targets` files have been added. A new class `ApplicationPackage` has been added to represent the contents of an application package. The `_build.csproj` has been updated to target .NET 8.0 and a new `_build.csproj.DotSettings` file has been added to configure ReSharper settings for the project.

---
+++ build/Build.cs
@@ -0,0 +1,18 @@
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+
+partial class Build : NukeBuild
+{
+    readonly AbsolutePath ArtifactsDirectory = RootDirectory / "output";
+
+    readonly string[] CompiledAssemblies = { "IsolateWarnings.dll" };
+
+    [Solution(GenerateProjects = true)] 
+    Solution Solution;
+
+    public static int Main () => Execute<Build>(x => x.Publish);
+
+    //[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    //readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+}
